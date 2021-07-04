a76cba76eed3fc7adedc2154016c11901150bf94
Igor <matviychukigor@gmail.com>
2021-07-04 21:47:41 +0300

    REACT sneakers 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-import Card from './components/Card'
+import Card from './components/Card/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
+
+const arr = [
+  {name: "Мужские кросовки NIKE Blazer Mid Surde", price: 12999, img:"/img/sneakers/1.jpg"},
+  {name: "Мужские кросовки NIKE AIR Max Surde", price: 15999, img:"/img/sneakers/2.jpg"},
+  {name: "Мужские кросовки NIKE AIR Max 725", price: 13999, img:"/img/sneakers/3.jpg"},
+  {name: "Мужские кросовки NIKE Blazer Max Surde", price: 10999, img:"/img/sneakers/4.jpg"},
+  {name: "Мужские кросовки NIKE Blazer Max Surde", price: 11999, img:"/img/sneakers/5.jpg"}
+];
 
 function App() {
   return (
@@ -19,8 +27,14 @@ function App() {
         </div>
         
         <div className="d-flex">
-
-          <Card/>
+          {arr.map((val) => (
+            <Card
+              name={val.name}
+              price={val.price}
+              img={val.img}
+            />
+          ))}
+          
           
         </div>
       </div>
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,19 +1,19 @@
-import React from 'react';
+import styles from "./Card.module.scss";
 
-function Card(){
+function Card({name, price, img}){
     return(
-        <div className="card">
-            <div className="favorite">
+        <div className={styles.card}>
+            <div className={styles.favorite}>
                 <img src="/img/unliked.svg" alt="unliked"/> 
             </div>
-            <img width={133} height={112} src="/img/sneakers/2.jpg" alt=""/>
-            <h5>Мужские кросовки NIKE Blazer Mid Surde</h5>
+            <img width={133} height={112} src={img} alt=""/>
+                <h5>{name}</h5>
             <div className="d-flex justify-between align-center">
                 <div className="d-flex flex-column">
                 <span>Цена:</span>
-                <b>12 999 грн.</b>
+                <b>{price} грн.</b>
                 </div>
-                <button className="button">
+                <button className="button" onClick={() => alert("123")}>
                 <img width={11} height={11} src="/img/plus.svg" alt="Plus"></img>
                 </button>
             </div>
